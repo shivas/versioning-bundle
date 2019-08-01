ea7b0d7d1e39c5fa383e95ef01c9e8cda850e291
Dominic Tubach <dominic.tubach@to.com>
2019-08-01 19:03:52 +0200

    ProviderCompilerPass: Use ::class notation

--- DependencyInjection/Compiler/ProviderCompilerPass.php
+++ DependencyInjection/Compiler/ProviderCompilerPass.php
@@ -2,6 +2,7 @@
 
 namespace Shivas\VersioningBundle\DependencyInjection\Compiler;
 
+use Shivas\VersioningBundle\Service\VersionManager;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -16,11 +17,11 @@ class ProviderCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if (!$container->hasDefinition('Shivas\VersioningBundle\Service\VersionManager')) {
+        if (!$container->hasDefinition(VersionManager::class)) {
             return;
         }
 
-        $definition = $container->getDefinition('Shivas\VersioningBundle\Service\VersionManager');
+        $definition = $container->getDefinition(VersionManager::class);
         $providers = $container->findTaggedServiceIds('shivas_versioning.provider');
 
         foreach ($providers as $id => $attributes) {
