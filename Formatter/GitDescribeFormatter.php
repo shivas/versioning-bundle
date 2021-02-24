7b02e22cdc0ecc4a2b0ad56862fe1df2a8249912
Kevin Lot <klot@keyclic.com>
2021-02-24 19:17:03 +0100

    Code review.

--- Formatter/GitDescribeFormatter.php
+++ Formatter/GitDescribeFormatter.php
@@ -2,7 +2,6 @@
 
 namespace Shivas\VersioningBundle\Formatter;
 
-use Version\Extension\PreRelease;
 use Version\Version;
 
 /**
@@ -19,22 +18,35 @@ class GitDescribeFormatter implements FormatterInterface
     public function format(Version $version): Version
     {
         $preRelease = $version->getPreRelease();
-        if (!$preRelease instanceof PreRelease) {
+        if (null === $preRelease) {
             return $version;
         }
 
         if (preg_match('/^(\d+)-g([a-fA-F0-9]{7,40})(-dirty)?$/', $preRelease->toString(), $matches)) {
-            $withPreRelease = (int) $matches[1] != 0 ? sprintf('dev.%s',  $matches[2]) : null;
-            $version = $version->withPreRelease($withPreRelease);
-        }
+            if ('0' !== $matches[1]) {
+                $withPreRelease = sprintf('dev.%s', $matches[2]);
+            } else {
+                $withPreRelease = null;
+            }
 
-        if (preg_match('/^(.*)-(\d+)-g([a-fA-F0-9]{7,40})(-dirty)?$/', $preRelease->toString(), $matches)) {
-            if ((int) $matches[2] != 0) {
+            $version = $version->withPreRelease($withPreRelease);
+        } elseif (preg_match('/^(.*)-(\d+)-g([a-fA-F0-9]{7,40})(-dirty)?$/', $preRelease->toString(), $matches)) {
+            if ('0' !== $matches[2]) {
                 // if we are not on TAG commit, add "dev" and git commit hash as pre release part
-                $withPreRelease = empty($matches[1]) ? sprintf('dev.%s',  $matches[3]) : sprintf('%s.dev.%s', trim($matches[1], '-'),  $matches[3]);
+                if (empty($matches[1])) {
+                    $withPreRelease = sprintf('dev.%s', $matches[3]);
+                } else {
+                    $withPreRelease = sprintf('%s.dev.%s', trim($matches[1], '-'), $matches[3]);
+                }
+
                 $version = $version->withPreRelease($withPreRelease);
             } else {
-                $withPreRelease = empty($matches[1]) ? null : trim($matches[1], '-');
+                if ('' === $matches[1]) {
+                    $withPreRelease = null;
+                } else {
+                    $withPreRelease = trim($matches[1], '-');
+                }
+
                 $version = $version->withPreRelease($withPreRelease);
             }
         }
