df8087112abcd4655eb9bc396ae90baf784e1eab
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-08-07 13:05:26 +0200

    Solve sf 7 deprecation

--- src/DependencyInjection/ShivasVersioningExtension.php
+++ src/DependencyInjection/ShivasVersioningExtension.php
@@ -4,7 +4,7 @@ namespace Shivas\VersioningBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 final class ShivasVersioningExtension extends Extension
